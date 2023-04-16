82e3ff43a8d451acaafb1cb9eccb21b908a3f36d
Marco Castillo <marcocastillo1997@gmail.com>
2023-04-16 00:28:19 -0600

    updating colors in navbar links

--- src/components/commons/NavbarLink.js
+++ src/components/commons/NavbarLink.js
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 const NavbarLink = ({ linkData, currentLocation }) => {
   return (
     <li
-      className={`${
-        currentLocation === linkData.path && "font-bold"
-      } mr-3 hover:underline`}
+      className={`${currentLocation === linkData.path && "font-bold underline"}
+       ${currentLocation === linkData.path ? "text-white" : "text-gray-200"} 
+      mr-3 hover:underline`}
     >
       <Link to={linkData.path}>{linkData.title}</Link>
     </li>
